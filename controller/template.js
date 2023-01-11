72470dc80912716f6cdd492312c47ab781c33781
Leon564 <nleon564.nl@gmail.com>
2023-01-11 13:34:52 -0600

    feat(): support to pdf

--- controller/template.js
+++ controller/template.js
@@ -1,28 +1,31 @@
 const JSZip = require("pizzip");
 const Docxtemplater = require("docxtemplater");
-const templates=require('../templates');
+const templates = require('../templates');
 const fs = require("fs");
 const path = require("path");
+const wordToPdf = require("../lib/docxToPdf");
 
-const mdoc = (req, res) => {
-  let data=req.query;  
-  if(!data.lvl || !data.name || !data.last_name || !data.day || !data.month || !data.year){
-   return res.send("error data");
+const mdoc = async (req, res) => {
+  let data = req.query;
+  if (!data.lvl || !data.name || !data.last_name || !data.day || !data.month || !data.year) {
+    return res.send("error data");
   }
-  if(data.name.length + data.last_name.length < 18){
-    data.last_name="  "+data.last_name.split(" ").join("  ");
-    data.name="\t"+data.name.split(" ").join("  ");;    
+  if (data.name.length + data.last_name.length < 18) {
+    data.last_name = "  " + data.last_name.split(" ").join("  ");
+    data.name = "\t" + data.name.split(" ").join("  ");;
   };
-  let file=templates[data.lvl];
-  console.log(file);
-  if(!file) return res.status(404).send("error template");
-  // Cargo el docx como un  binary
-  var content = fs.readFileSync(path.resolve(file), "binary");
-    //let zip= JSZip(content);
-   
-  var zip = new JSZip(content);
+  let file = templates[data.lvl];
 
-  var doc = new Docxtemplater();
+  console.log(file);
+
+  if (!file) return res.status(404).send("error template");
+  // Cargo el docx como un  binary
+  let content = fs.readFileSync(path.resolve(file), "binary");
+  //let zip= JSZip(content);
+
+  let zip = new JSZip(content);
+
+  let doc = new Docxtemplater();
   doc.loadZip(zip);
 
   // setea los valores de data ej: { first_name: 'John' , last_name: 'Doe'}
@@ -32,7 +35,7 @@ const mdoc = (req, res) => {
     // renderiza el documento (remplaza las ocurrencias como {first_name} by John, {last_name} by Doe, ...)
     doc.render();
   } catch (error) {
-    var e = {
+    let e = {
       message: error.message,
       name: error.name,
       stack: error.stack,
@@ -42,14 +45,41 @@ const mdoc = (req, res) => {
     throw error;
   }
 
-  var buffer = doc.getZip().generate({ type: "nodebuffer" });
-  const fileName = `${data.name} ${data.last_name}.docx`
-  const fileType = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+  let buffer = doc.getZip().generate({ type: "nodebuffer" });
+
+  
+
+  const fileName = `${data.name} ${data.last_name}${data.pdf ? '.pdf' : '.docx'}`
+
+
+  if (!data.pdf) {
+    console.log("converting docx");
+    const fileType = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    res.writeHead(200, {
+      'Content-Disposition': `attachment; filename="${fileName}"`,
+      'Content-Type': fileType,
+    })
+
+    return res.end(buffer);
+
+  }
+
+  console.log("converting pdf");
+  const filetype = 'application/pdf';
+
+  const pdfBuffer = await wordToPdf(buffer);
+
+  if (pdfBuffer.error) {
+    return res.status(500).send("error converting pdf");
+  }
 
   res.writeHead(200, {
     'Content-Disposition': `attachment; filename="${fileName}"`,
-    'Content-Type': fileType,
+    'Content-Type': filetype,
   })
-  return res.end(buffer);
+
+  return res.end(pdfBuffer); 
+
+
 };
 module.exports = mdoc;
